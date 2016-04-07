be6f6eda718d9e023e56c3cda838703cdf8deac0
Jason Ebbin <jebbin@scottlogic.co.uk>
2016-04-07 11:04:12 +0100

    Commit basic Project assets copied and modified from the sessions service

---
+++ Profiles/Program.fs
@@ -0,0 +1,7 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main _ = 
+    printfn "Initial Project Commit"
+    0 // return an integer exit code
