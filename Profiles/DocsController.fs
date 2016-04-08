33ce2a52798d9a184fce906a974fd6c14cddcc83
ilovell <ilovell@scottlogic.co.uk>
2016-04-08 11:10:27 +0100

    Added docs controller and initial API docs

---
+++ Profiles/DocsController.fs
@@ -0,0 +1,17 @@
+﻿module DocsController
+
+open Newtonsoft.Json.Linq
+open System.IO
+open System.Net
+open System.Net.Http
+open System.Web.Http
+
+type DocsController() =
+    inherit ApiController()
+
+    member x.Get() =        
+        use stream = x.GetType().Assembly.GetManifestResourceStream("api.json")
+        use streamReader = new StreamReader(stream)
+        let content = streamReader.ReadToEnd()
+        let docs = JObject.Parse(content);        
+        x.Request.CreateResponse(HttpStatusCode.OK, docs, "application/json")
